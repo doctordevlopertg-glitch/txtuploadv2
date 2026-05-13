3a92c51d82f45e3e930e4792f51cf8e7c4d756ff
doctordevlopertg-glitch <doctordevlopertg@gmail.com>
2026-05-13 17:24:48 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,13 +3,13 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22946135")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "93e1b0c387cabe34a3ccfa1724ae8527") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "39683282")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "ab1cc41ca283d480ebe386b1dce182f3") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "8698962753:AAFZ1MJuLwucVbZQi_xUbKDP01Za6cukp-A") #Replace with your bot token
 
 # Database Configuration for MongoDB
 # Example: "mongodb://user:password@host:port/dbname"
 # For MongoDB Atlas: "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/<dbname>?retryWrites=true&w=majority"
-DATABASE_URL = environ.get("DATABASE_URL", "mongodb://localhost:27017/your_bot_db")
+DATABASE_URL = environ.get("mongodb+srv://doctodevlopertg:doctorPro#1@cluster0.dbexak8.mongodb.net/?appName=Cluster0", "mongodb://localhost:27017/your_bot_db")
 # Replace "your_bot_db" with your desired database name.
 # For local testing, you might use "mongodb://localhost:27017/your_bot_db"
